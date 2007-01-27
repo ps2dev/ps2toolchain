94a9dbcbc74dc0489ab7e3b1def84f33be16795d
lukasz <lukasz@fd1d1198-7684-4768-b9be-5b3c44f97897>
2007-01-27 20:39:42 +0000

    fix commented exports

--- toolchain.sh
+++ toolchain.sh
@@ -7,9 +7,9 @@
  ########################
 
   ## Main ps2dev settings.
-#  export PS2DEV="/usr/local/ps2dev"
-#  export PS2SDK="$PS2DEV/ps2sdk"
-#  export PATH="$PATH:$PS2DEV/bin:$PS2DEV/ee/bin:$PS2DEV/iop/bin:$PS2DEV/dvp/bin:$PS2SDK/bin"
+  export PS2DEV="/usr/local/ps2dev"
+  export PS2SDK="$PS2DEV/ps2sdk"
+  export PATH="$PATH:$PS2DEV/bin:$PS2DEV/ee/bin:$PS2DEV/iop/bin:$PS2DEV/dvp/bin:$PS2SDK/bin"
 
   ## Set the directories.
   export SRCDIR="`pwd`"
