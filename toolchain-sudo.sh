4ec44584a1a73f4203043b2ac5dde4945dec5a97
oopo <oopo@fd1d1198-7684-4768-b9be-5b3c44f97897>
2007-06-18 01:11:16 +0000

    Magically, you can now specify which steps to run.

--- toolchain-sudo.sh
+++ toolchain-sudo.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 # toolchain-sudo.sh by Dan Peori (danpeori@oopo.net)
 
- ## Enter the script directory.
- cd "`dirname $0`" || { echo "ERROR: Could not enter the script directory."; exit 1; }
+ ## Enter the ps2toolchain directory.
+ cd "`dirname $0`" || { echo "ERROR: Could not enter the ps2toolchain directory."; exit 1; }
 
  ## Set up the environment.
  export PS2DEV=/usr/local/ps2dev
@@ -14,4 +14,4 @@
  export PATH=$PATH:$PS2SDK/bin
 
  ## Run the toolchain script.
- ./toolchain.sh || { echo "ERROR: Could not run the toolchain script."; exit 1; }
+ ./toolchain.sh $@ || { echo "ERROR: Could not run the toolchain script."; exit 1; }
--- toolchain.sh
+++ toolchain.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 # toolchain.sh by Dan Peori (danpeori@oopo.net)
 
- ## Enter the script directory.
- cd "`dirname $0`" || { echo "ERROR: Could not enter the script directory."; exit 1; }
+ ## Enter the ps2toolchain directory.
+ cd "`dirname $0`" || { echo "ERROR: Could not enter the ps2toolchain directory."; exit 1; }
 
  ## Create the build directory.
  mkdir -p build || { echo "ERROR: Could not create the build directory."; exit 1; }
@@ -10,8 +10,24 @@
  ## Enter the build directory.
  cd build || { echo "ERROR: Could not enter the build directory."; exit 1; }
 
- ## Run the depend scripts.
- for SCRIPT in `ls ../depends/*.sh | sort`; do $SCRIPT || { echo "$SCRIPT: Failed."; exit 1; } done
+ ## Fetch the depend scripts.
+ DEPEND_SCRIPTS=(`ls ../depends/*.sh | sort`)
 
- ## Run the build scripts.
- for SCRIPT in `ls ../scripts/*.sh | sort`; do "$SCRIPT" || { echo "$SCRIPT: Failed."; exit 1; } done
+ ## Run all the depend scripts.
+ for SCRIPT in ${DEPEND_SCRIPTS[@]}; do "$SCRIPT" || { echo "$SCRIPT: Failed."; exit 1; } done
+
+ ## Fetch the build scripts.
+ BUILD_SCRIPTS=(`ls ../scripts/*.sh | sort`)
+
+ ## If specific steps were requested...
+ if [ $1 ]; then
+
+  ## Run the requested build scripts.
+  for STEP in $@; do "${BUILD_SCRIPTS[$STEP-1]}" || { echo "${BUILD_SCRIPTS[$STEP-1]}: Failed."; exit 1; } done
+
+ else
+
+  ## Run the all build scripts.
+  for SCRIPT in ${BUILD_SCRIPTS[@]}; do "$SCRIPT" || { echo "$SCRIPT: Failed."; exit 1; } done
+
+ fi
