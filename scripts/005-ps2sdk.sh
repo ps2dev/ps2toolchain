8d008710fdc37a729a63c89a6b158937ecb9d1f4
Mathias Lafeldt <misfire@debugon.org>
2010-12-02 00:33:50 +0100

    Unset PS2SDKSRC before building ps2sdk

--- scripts/005-ps2sdk.sh
+++ scripts/005-ps2sdk.sh
@@ -2,6 +2,9 @@
 # ps2sdk.sh by Dan Peori (danpeori@oopo.net)
 # changed to use Git by Mathias Lafeldt <misfire@debugon.org>
 
+# make sure ps2sdk's makefile does not use it
+unset PS2SDKSRC
+
  ## Download the source code.
  if test ! -d "ps2sdk"; then
   git clone git://github.com/ps2dev/ps2sdk.git && cd ps2sdk || exit 1
