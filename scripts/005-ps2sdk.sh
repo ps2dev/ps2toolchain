3d21169f7bc69518a43b7486adb73adead9cdaaf
Ildar Mulyukov <ildar@altlinux.ru>
2012-11-23 15:15:43 +0600

    adapt git fetched repos to $DOWNLOAD_DIR

--- scripts/005-ps2sdk.sh
+++ scripts/005-ps2sdk.sh
@@ -5,14 +5,20 @@
 # make sure ps2sdk's makefile does not use it
 unset PS2SDKSRC
 
+export GIT_DIR="$DOWNLOAD_DIR/ps2sdk/.git"
+
  ## Download the source code.
- if test ! -d "ps2sdk"; then
-  git clone git://github.com/ps2dev/ps2sdk.git && cd ps2sdk || exit 1
+ if test ! -d "$GIT_DIR"; then
+  git clone -n git://github.com/ps2dev/ps2sdk.git "$DOWNLOAD_DIR/ps2sdk/" || \
+    exit 1
  else
-  cd ps2sdk &&
-  git fetch origin &&
-  git reset --hard origin/master || exit 1
+  git fetch origin
  fi
+
+  rm -rf ps2sdk &&
+  mkdir ps2sdk &&
+  cd ps2sdk &&
+  git reset --hard origin/master || exit 1
 
  ## Build and install.
  make clean && make -j 2 && make release && make clean || { exit 1; }
--- scripts/006-ps2client.sh
+++ scripts/006-ps2client.sh
@@ -2,14 +2,20 @@
 # ps2client.sh by Dan Peori (danpeori@oopo.net)
 # changed to use Git by Mathias Lafeldt <misfire@debugon.org>
 
+export GIT_DIR="$DOWNLOAD_DIR/ps2client/.git"
+
  ## Download the source code.
- if test ! -d "ps2client"; then
-  git clone git://github.com/ps2dev/ps2client.git && cd ps2client || exit 1
+ if test ! -d "$GIT_DIR"; then
+  git clone -n git://github.com/ps2dev/ps2client.git "$DOWNLOAD_DIR/ps2client/" || \
+    exit 1
  else
-  cd ps2client &&
-  git fetch origin &&
-  git reset --hard origin/master || exit 1
+  git fetch origin
  fi
+
+  rm -rf ps2client &&
+  mkdir ps2client &&
+  cd ps2client &&
+  git reset --hard origin/master || exit 1
 
  ## Build and install.
  make clean && make && make install && make clean || { exit 1; }
