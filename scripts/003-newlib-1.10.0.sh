53152f25ed3481f52caab1a8eaea5707429191b1
sp193 <ysai187@yahoo.com>
2013-10-19 18:52:39 +0800

    Reverted the update to newlib. There's a problem with duplicated symbols like "strtol", and I cannot commit the time and energy to fix it. Not to mention that I have no idea why that now happens after the upgrade. :(

--- scripts/003-newlib-1.10.0.sh
+++ scripts/003-newlib-1.10.0.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-# newlib-1.13.0.sh
+# newlib-1.10.0.sh by Dan Peori (danpeori@oopo.net)
 
  ## Download the source code.
- SOURCE=ftp://sourceware.org/pub/newlib/newlib-1.13.0.tar.gz
+ SOURCE=https://github.com/downloads/ps2dev/ps2toolchain/newlib-1.10.0.tar.gz
  wget --continue --no-check-certificate $SOURCE || { exit 1; }
 
  ## Unpack the source code.
- rm -Rf newlib-1.13.0 && tar xfz newlib-1.13.0.tar.gz || { exit 1; }
+ rm -Rf newlib-1.10.0 && tar xfvz newlib-1.10.0.tar.gz || { exit 1; }
 
  ## Enter the source directory and patch the source code.
- cd newlib-1.13.0 && cat ../../patches/newlib-1.13.0-PS2.patch | patch -p1 || { exit 1; }
+ cd newlib-1.10.0 && cat ../../patches/newlib-1.10.0-PS2.patch | patch -p1 || { exit 1; }
 
  ## Create and enter the build directory.
  mkdir build-ee && cd build-ee || { exit 1; }
