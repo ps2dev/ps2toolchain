061f10b3eea8867063bfab68724b7f0d72907eba
AKuHAK <AKuHAK@users.noreply.github.com>
2018-12-24 12:17:28 +0200

    Update 003-newlib-1.10.0.sh
    Typo in newlib

--- scripts/003-newlib-1.10.0.sh
+++ scripts/003-newlib-1.10.0.sh
@@ -3,7 +3,7 @@
 
 NEWLIB_VERSION=1.10.0
 ## Download the source code.
-SOURCE=http://mirrors.kernel.org/sourceware/newlib/newlib-1.11.0.tar.gz
+SOURCE=http://mirrors.kernel.org/sourceware/newlib/$NEWLIB_VERSION.tar.gz
 wget --continue $SOURCE || { exit 1; }
 
 ## Unpack the source code.
