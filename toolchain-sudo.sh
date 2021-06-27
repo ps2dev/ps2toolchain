60003b60c3ab8655bf1eac96d447c1b2e63ca4ea
AKuHAK <621640+AKuHAK@users.noreply.github.com>
2021-06-27 11:41:34 +0300

    Fixed scripts not producing failing code Fixed scripts to handle complex paths and tags Various cleanups

--- toolchain-sudo.sh
+++ toolchain-sudo.sh
@@ -2,7 +2,7 @@
 # toolchain-sudo.sh by Naomi Peori (naomi@peori.ca)
 
 ## Enter the ps2toolchain directory.
-cd "`dirname $0`" || { echo "ERROR: Could not enter the ps2toolchain directory."; exit 1; }
+cd "$(dirname "$0")" || { echo "ERROR: Could not enter the ps2toolchain directory."; exit 1; }
 
 ## Set up the environment.
 export PS2DEV=/usr/local/ps2dev
@@ -14,4 +14,4 @@ export PATH=$PATH:$PS2DEV/dvp/bin
 export PATH=$PATH:$PS2SDK/bin
 
 ## Run the toolchain script.
-./toolchain.sh $@ || { echo "ERROR: Could not run the toolchain script."; exit 1; }
+./toolchain.sh "$@" || { echo "ERROR: Could not run the toolchain script."; exit 1; }
